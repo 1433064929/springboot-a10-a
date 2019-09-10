dc8f3ee62a982e59f678d430a2a4c0b192dac5b3
bb <aaa.163.com>
2019-09-10 19:55:32 +0800

    add

---
+++ src/main/java/com/woniuxy/springboot/SpringbootA10AApplication.java
@@ -0,0 +1,13 @@
+package com.woniuxy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootA10AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootA10AApplication.class, args);
+	}
+
+}
